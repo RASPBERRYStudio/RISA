a46b87407c82e5b77d120308a12fde9bd7509cb6
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-02-25 23:31:55 +0900

    add toot

---
+++ modules/toot.js
@@ -0,0 +1,19 @@
+const conf = require('../config');
+const Mastodon = require('mastodon-api')
+module.exports = {
+  toot: function (status, visibility = conf.mastodon.default_visibility) {
+    const mstdn = new Mastodon({
+      api_url: 'https://' + conf.mastodon.url + '/api/v1/',
+      access_token: conf.mastodon.access_token
+    });
+    mstdn.post('statuses', { status: status, visibility: visibility }, function (err, data, response) {
+      if (err){
+        console.error(err);
+        return false;
+      }
+      console.log(data);
+      console.log(response);
+      return true;
+    })
+  }
+}
