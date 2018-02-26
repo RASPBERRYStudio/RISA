9ccd4577b95d005184a4fbf88565f064ad24e266
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-02-26 16:54:20 +0900

    add config sample

---
+++ config.sample.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mastodon: {
+    url: 'mstdn.y-zu.org',
+    access_token: 'token',
+    default_visibility: 'direct',
+    id: '@RISA'
+  },
+  docomo: {
+    url: 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue',
+    apikey: 'apikey'
+  },
+  google: {
+    geokey: 'apikey'
+  },
+  app: {
+    missing: '見つからなかったよ....',
+    error: 'よくわからなくなっちゃった....'
+  }
+}
