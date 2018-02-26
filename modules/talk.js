995e13425ae345a08942f8264eaa1b6bb6e51591
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-02-26 16:27:51 +0900

    toot.js変更に伴う変更

--- modules/talk.js
+++ modules/talk.js
@@ -3,33 +3,30 @@ const fs = require('fs');
 const path = require('path');
 const conf = require('../config');
 const mstdn = require('./toot');
-let listener = mstdn.startStream();
-listener.on('message', msg => {
-  if (msg.event === 'notification') {
-    let content = msg.data.status.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').replace(conf.mastodon.id, '');
-    let acct = msg.data.account.acct;
-    var options = {
-      uri: `${conf.docomo.url}?APIKEY=${conf.docomo.apikey}`,
-      body: {
-        utt: content,
-        nickname: msg.data.account.display_name,
-        context: getContext(acct),
-        t: 20
-      },
-      json: true
-    };
-    request.post(options).then(function (res) {
-      let context = res.context;
-      fs.writeFileSync(`${__dirname}/../contexts/${acct}`, context);
-      mstdn.toot(`@${acct} ${res.utt}`)
-      }).catch(function (err) {
-      if (err) {
-        console.error(err);
-        mstdn.toot(`@${acct} ？`)
-      }
-    });
-  }
-})
+module.exports.main = function (msg) {
+  let content = msg.data.status.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').replace(conf.mastodon.id, '');
+  let acct = msg.data.account.acct;
+  var options = {
+    uri: `${conf.docomo.url}?APIKEY=${conf.docomo.apikey}`,
+    body: {
+      utt: content,
+      nickname: msg.data.account.display_name,
+      context: getContext(acct),
+      t: 20
+    },
+    json: true
+  };
+  request.post(options).then(function (res) {
+    let context = res.context;
+    fs.writeFileSync(`${__dirname}/../contexts/${acct}`, context);
+    mstdn.toot(`@${acct} ${res.utt}`, msg.data.status.id)
+    }).catch(function (err) {
+    if (err) {
+      console.error(err);
+      mstdn.toot(`@${acct} ${conf.app.error}`, msg.data.status.id)
+    }
+  });
+}
 
 function getContext(acct) {
   let context;
