8f1a46bf5ff9be56aadd81b655019dfe2171613b
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-02-26 21:54:47 +0900

    にゃーん

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const mstdn = require('./modules/toot');
 let listener = mstdn.startStream();
 listener.on('message', msg => {
   if (msg.event === 'notification') {
-    let content = msg.data.status.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').replace(conf.mastodon.id, '');
-    if (content.match(/のなか卯/g)) {
+    let content = msg.data.status.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').replace(conf.mastodon.id, '').replace(/\s/g, '');
+    let acct = msg.data.account.acct;
+    if (content === 'にゃーん') {
+      mstdn.toot(`@${acct} にゃーん`, msg.data.status.id)
+    } else if (content.match(/のなか卯/g)) {
       require('./modules/nakau').main(msg);
     } else if (content.match(/地図|マップ/g)) {
       require('./modules/map').main(msg);
