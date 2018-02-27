61d2a8aa4f06ee652bcf11371b951c977e52d304
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-02-27 15:23:23 +0900

    重大な不具合の修正 メンション以外にも反応していた不具合

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const mstdn = require('./modules/toot');
 let listener = mstdn.startStream();
 listener.on('message', msg => {
   if (msg.event === 'notification') {
-    let content = msg.data.status.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').replace(conf.mastodon.id, '').replace(/\s/g, '');
-    let acct = msg.data.account.acct;
-    if (content === 'にゃーん') {
-      mstdn.toot(`@${acct} にゃーん`, msg.data.status.id)
-    } else if (content.match(/のなか卯/g)) {
-      require('./modules/nakau').main(msg);
-    } else if (content.match(/地図|マップ/g)) {
-      require('./modules/map').main(msg);
-    } else {
-      require('./modules/talk').main(msg);
+    console.log(msg.data.status.account.acct);
+  }
+  if (msg.event === 'notification' && msg.data.status.account.acct !== conf.mastodon.id) {
+    if(msg.data.type === 'mention'){
+      let content = msg.data.status.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').replace(conf.mastodon.id, '').replace(/\s/g, '');
+      let acct = msg.data.account.acct;
+      if (content === 'にゃーん') {
+        mstdn.toot(`@${acct} にゃーん`, msg.data.status.id)
+      } else if (content.match(/のなか卯/g)) {
+        require('./modules/nakau').main(msg);
+      } else if (content.match(/地図|マップ/g)) {
+        require('./modules/map').main(msg);
+      } else {
+        require('./modules/talk').main(msg);
+      }
     }
   }
 })
